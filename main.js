97b28233f627f5146f660de58bdf96002f347a66
siva <sivashakthiarun.c@gmail.com>
2022-09-13 10:33:57 +0530

    repush

---
+++ main.js
@@ -0,0 +1,17 @@
+/*var fabevy=[1,2,3,4,5,6,"Shakthi"];
+console.log("Original array:",fabevy);
+console.log("Length of original array:",fabevy.length);
+var a=fabevy.copyWithin(1,3);
+console.log("Method:copyWithin(1,3);");
+console.log("Change in array:",a);
+console.log("Return:Length of the array after copyWithin(); is"+a.length);
+*/
+
+var fabevy=[1,2,3,4,5,6,"Shakthi"];
+console.log("Original array:",fabevy);
+console.log("Length of original array:",fabevy.length);
+var a=fabevy.copyWithin(1,3,5);
+console.log("Method:copyWithin(1,3,5);");
+console.log("Change in array:",a);
+console.log("Return:Length of the array after copyWithin(); is"+a.length);
+
